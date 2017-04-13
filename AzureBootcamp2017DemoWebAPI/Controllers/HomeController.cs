d48b6b961002724e87b787759c20050e1e1b8ac3
Gabriel Molter <gabriel.molter@gmail.com>
2017-04-13 00:09:08 -0300

    Added localization button and changed Images

--- AzureBootcamp2017DemoWebAPI/Controllers/HomeController.cs
+++ AzureBootcamp2017DemoWebAPI/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AzureBootcamp2017DemoWebAPI.helpers;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +14,6 @@ namespace AzureBootcamp2017DemoWebAPI.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.EnvironmentLocation = "Error";
 
             try
             {
@@ -20,16 +22,41 @@ namespace AzureBootcamp2017DemoWebAPI.Controllers
                 if (config != null)
                 {
                     string env = config.ToString();
-                     ViewBag.EnvironmentLocation = env;
+                    ViewBag.EnvironmentLocation = env;
+
+                    ViewBag.EnvironmentLocationCss = readJsonConfig(env);
                 }
 
             }
             catch(Exception)
             {
-
+                ViewBag.EnvironmentLocation = "Error";
+                ViewBag.EnvironmentLocationCss = "default";
             }
 
             return View();
+        }
+
+        private string readJsonConfig(string env)
+        {
+            try
+            {
+                var json = System.IO.File.ReadAllText(Server.MapPath(@"~/App_Data/RegionColorMapping.json"));
+                List<ButtonRegion> items = JsonConvert.DeserializeObject<List<ButtonRegion>>(json);
+
+                var region = items.Find(x => x.RegionName == env);
+
+                if(region == null)
+                {
+                    return "default";
+                }
+
+                return region.Color;
+                
+            }catch (Exception e)
+            {
+                return "default";
+            }
         }
     }
 }
